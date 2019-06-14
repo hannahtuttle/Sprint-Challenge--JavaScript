e5cf86a7f007acbf171dc700f7d912b87ffcd15c
Hannah Tuttle <hannah@Hannahs-Air.PK5001Z>
2019-06-14 08:35:12 -0700

    finished request2 arrays

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -81,7 +81,9 @@ The resulting contact information should have a space between the first name and
 Name email@example.com
 
 Log the result of your new array. */
-const contactInfo = [];
+const contactInfo = graduates.map((contact) => {
+  return contact.first_name + ' ' + contact.email
+});
 console.log(contactInfo);
 
 
